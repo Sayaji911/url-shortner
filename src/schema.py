3d674cc9c8dbe086409282c1a28e652fe0e2419f
Sayaji911 <sayajichavan9999@gmail.com>
2021-05-03 21:47:02 +0530

    Add files via upload

--- src/schema.py
+++ src/schema.py
@@ -1,10 +1,5 @@
-from mongoengine import Document, StringField
-from bson import ObjectId
-from datetime import datetime
+from pydantic import BaseModel
 
 
-class UrlSchema(Document):
-    id = ObjectId()
-    long_Url = StringField(required=True)
-    short_Url = StringField(required=True)
-    nano_COde = StringField(required=True, unique=True)
+class UrlSchema(BaseModel):
+    long_Url = str
