828eea11e1907bf815da624726991f9577245a95
Sayaji911 <sayajichavan9999@gmail.com>
2021-05-04 01:23:55 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
-from fastapi import FastAPI
-from src.schema import UrlSchema
-from src.models import Url
-import os
-import nanoid
+from fastapi import FastAPI, Request, Depends
+from src.shortit import src as shortensrc
+from mongoengine import connect, disconnect, errors
+from decouple import config
 
 
 app = FastAPI()
-'''@app.post("/")
-async def test(url: UrlSchema):
-    url = url
-    nano_Code = nanoid.generate(size=8)
-    short_Url = os.path.join("BASE_URL", nano_Code)
 
-    url = Url(
-        long_Url=url["long_Url"],
-        nano_Code=nano_Code,
-        short_Url=short_Url
-    )
-    url.save()
-'''
+MODEL_URL = config("MODEL_URL")
 
-@app.post("/", response_model= dict)
-def shorty(url : UrlSchema):
 
-    url = dict(url)
-    nano_Code = nanoid.generate(size=8)
-    short_Url = os.path.join("BASE_URL", nano_Code)
-    return short_Url
+@app.get("/")
+async def index(request: Request):
+    app.include_router(shortensrc)
 
+
+@app.on_event("startup")
+async def create_db_client():
+    try:
+        connect(host = MODEL_URL )
+        print("Connected")
+
+    except Exception as e:
+        print(e)
+        print("An error occured")
+
+@app.on_event("shutdown")
+async  def shutdown_db_client():
+    pass
